453c8c3955150e723ec773d1e73bc359f5819abb
Jose-Amerval <j.amerval95@gmail.com>
2022-10-20 19:08:29 -0300

    Imprimindo na tela os valores das constantes e resultados

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício2.js
@@ -1,7 +1,8 @@
 const base = 5;
-
+console.log(base);
 const heigth = 8;
-
+console.log(heigth);
 const area = base * heigth;
-
+console.log(area);
 const perimeter = 2*base + 2*heigth;
+console.log(perimeter);
