276da8844bf4022de3245758450e92a424b09416
Jose-Amerval <j.amerval95@gmail.com>
2022-10-21 15:39:19 -0300

    Fazendo exercício de triângulo

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio11.js
@@ -0,0 +1,13 @@
+const angulo1 = 20;
+const angulo2 = 120;
+const angulo3 = 40;
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log('true');
+
+} else if (angulo1 + angulo2 + angulo3 > 180) {
+    console.log('false');
+
+} else {
+    console.log('inválido!')
+}
