a7d1f4f586784b9913e95c0a69dbdcd44018a24f
Jose-Amerval <j.amerval95@gmail.com>
2022-10-21 09:27:54 -0300

    Definindo o maior entre 3 números com condicionais

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,16 @@
+const a = 0;
+const b = 3;
+const c = 8;
+
+if (a > b > c) {
+    console.log('A é o maior de todos');
+
+} else if (b > a > c) {
+    console.log('B é o maior de todos');
+
+} else if (c > b > a) {
+    console.log('C é o maior de todos');
+
+} else {
+    console.log('A, B e C são iguais!');
+}
