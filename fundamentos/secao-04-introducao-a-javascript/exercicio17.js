2cb7fc6b398e24cf0b6a9609493d43ef6bc83ee1
Jose-Amerval <j.amerval95@gmail.com>
2022-10-23 17:33:01 -0300

    Calculando aliquotaINSS do salário 1500

---
+++ fundamentos/secao-04-introducao-a-javascript/exercicio17.js
@@ -0,0 +1,18 @@
+
+let aliquotaINSS;
+let aliquotaIR;
+
+const salarioBruto = 1500.00;
+
+if (salarioBruto <= 1556.94) {
+  aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  aliquotaINSS = salarioBruto * 0.11;
+} else {
+  aliquotaINSS = 570.88;
+}
+console.log(aliquotaINSS);
+
+
