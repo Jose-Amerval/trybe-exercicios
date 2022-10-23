f7e24a51632a6498e21e9df64e57074f20dba37f
Jose-Amerval <j.amerval95@gmail.com>
2022-10-23 09:24:49 -0300

    executando a soma dos angulos internos e verificando se algum é negativo ou se é >= 180

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio11.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio11.js
@@ -3,11 +3,13 @@ const angulo2 = 120;
 const angulo3 = 40;
 
 if (angulo1 + angulo2 + angulo3 === 180) {
-    console.log('true');
+   console.log('True');
 
-} else if (angulo1 + angulo2 + angulo3 > 180) {
-    console.log('false');
+} else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+    console.log('False');  
 
 } else {
-    console.log('inválido!')
+    console.log('Você não é um triângulo!')
 }
+
+ 
