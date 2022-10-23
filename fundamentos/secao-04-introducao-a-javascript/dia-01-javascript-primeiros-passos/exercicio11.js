a6d1be21f5435f4c0a426cb3c3d4a689706cb079
Jose-Amerval <j.amerval95@gmail.com>
2022-10-23 09:42:04 -0300

    Arrumando estrutura condicional linha 5

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio11.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio11.js
@@ -2,7 +2,7 @@ const angulo1 = 20;
 const angulo2 = 120;
 const angulo3 = 40;
 
-if (angulo1 + angulo2 + angulo3 === 180) {
+if (angulo1 + angulo2 + angulo3 === 180 && angulo1 > 0 && angulo2 > 0 && angulo3 > 0 ) {
    console.log('True');
 
 } else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
