94418c24cf65bac35587d4df354c88fc76962745
Jose-Amerval <j.amerval95@gmail.com>
2022-10-23 10:19:14 -0300

    Utilizando switch and case para verificar peça e imprimindo seu movimento no console.

--- fundamentos/secao-04-introducao-a-javascript/exercicio12.js
+++ fundamentos/secao-04-introducao-a-javascript/exercicio12.js
@@ -1,1 +1,25 @@
-const pecaDeXadrez = 'cavalo'
+const pecaDeXadrez =  'cavalo'
+switch (pecaDeXadrez.toLowerCase()) {
+    case 'cavalo':
+        console.log('Cavalo -> Move-se em L e pula peças.');
+        break;
+    case 'rei':
+        console.log('Rei ->  Move-se para qualquer direção apenas uma casa.');
+        break;
+    case 'bispo':
+        console.log('Bispo -> Move-se em diagonal quantas casas quiser.');
+        break;
+    case 'rainha':
+        console.log('Rainha -> Move-se para qualquer direção quantas casas quiser.')
+        break;
+    case 'torre': 
+        console.log('Torre -> Move-se em horizontal e vertical quantas casas quiser se não estiver ocupada.');
+        break;
+    case 'peão':
+        console.log('Peão -> Move-se para frente apenas uma casa.')
+        break;
+        default:
+          console.log('Erro, peça não pertence ao xadrez!'); 
+          
+    }      
+
