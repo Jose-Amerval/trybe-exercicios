bedd785c32fffd67c2534d4002b6b894f53d4760
Jose-Amerval <j.amerval95@gmail.com>
2022-10-24 19:15:12 -0300

    Arrumando a estrutura condicional linha 3,4 e colocando log fora da estrutura condicional

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -1,5 +1,7 @@
 let word ='tryber';
-for (let index = 5; index > 0; index -= 1) {
- console.log(word[index]);
+let wordInvertida = '';
+for (let index = 5; index >= 0; index -= 1) {
+ wordInvertida = wordInvertida + word[index];
  
 }
+console.log(wordInvertida)
