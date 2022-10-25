54a1a0beffb43ec100a7f38b7e40484ceb96897b
Jose-Amerval <j.amerval95@gmail.com>
2022-10-25 18:32:21 -0300

    Criando 5 programas que retornem o resultado das operações

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funçoes/exercicio4.js
@@ -0,0 +1,25 @@
+function adicao(a, b) {
+    return a + b;
+
+}
+console.log(adicao(5, 6));
+
+function subtracao(a, b) {
+    return a - b;
+}
+console.log(subtracao(10, 5));
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(10, 7));
+
+function divisao(a, b) {
+    return a / b;
+}
+console.log(divisao(50, 2))
+
+function modolo(a, b) {
+    return a % b;
+}
+console.log(modolo(50, 10))
