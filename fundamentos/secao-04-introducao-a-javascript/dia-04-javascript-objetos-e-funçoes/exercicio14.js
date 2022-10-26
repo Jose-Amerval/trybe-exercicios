e4ea843890116b5b8d9f831b392cc54783ea332e
Jose-Amerval <j.amerval95@gmail.com>
2022-10-26 12:48:01 -0300

    Nominando função pra ver qual o numero mais repetido

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funçoes/exercicio14.js
@@ -0,0 +1,9 @@
+function maisRepetido(numeros) {
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+
+    for (let index in numeros) {
+        let verificaNumero = numeros[index];
+      }
+    }
