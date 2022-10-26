0db532d5fde9ee1fcb1c5eae1c0e79a46def91f0
Jose-Amerval <j.amerval95@gmail.com>
2022-10-25 22:14:37 -0300

    Imprimindo os valores de cada objeto juntos de acordo com cada uma das chaves

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funçoes/exercicio9.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funçoes/exercicio9.js
@@ -14,12 +14,22 @@ let info = {
   }
 
   for (let key in info) {
-    console.log(info[key]);
+    // console.log(info[key]);
   }
 
   let info2 = {
     personagem: 'Tio Patinhas',
     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178', 
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    nota: 'O último MacPatinhas',
     recorrente: 'Sim'
 };
+
+for (let key in info) {
+    if (key === 'recorrente' &&
+    info[key] === 'Sim' && 
+    info2[key] === 'Sim') {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + ' e ' + info2[key]);
+    }
+}
